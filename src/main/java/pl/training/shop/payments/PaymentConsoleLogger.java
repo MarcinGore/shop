1a8f5f1ff975508b2c80036bf9c3bb5e3107b646
Marcin <marcinsun@gmail.com>
2021-03-27 13:20:51 +0100

    AOP different advice

--- src/main/java/pl/training/shop/payments/PaymentConsoleLogger.java
+++ src/main/java/pl/training/shop/payments/PaymentConsoleLogger.java
@@ -2,8 +2,7 @@ package pl.training.shop.payments;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.*;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +17,21 @@ public class PaymentConsoleLogger {
     private static final String MESSAGE_KEY="paymentInfo";
 
     private final MessageSource messageSource;
+
+    @Before(value = "@annotation(LogPayments) && args(paymentRequest)")
+    public void beforePayment(PaymentRequest paymentRequest){
+        log.info("New payment request :" +paymentRequest);
+    }
+
+    @After("@annotation(LogPayments)")
+    public void afterPayment(){
+        log.info("After payment");
+    }
+
+    @AfterThrowing(value = "@annotation(LogPayments)",throwing = "exception")
+    public void onException(RuntimeException exception){
+        log.info("Payment exception "+exception.getMessage());
+    }
 
     @AfterReturning(value = "@annotation(LogPayments)", returning = "payment")
     public void log(Payment payment){
